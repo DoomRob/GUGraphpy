df6ea6de9a77b4826f33bc07f9d450fa3cb35894
Robert Gannon <123395116+DoomRob@users.noreply.github.com>
2024-12-08 13:26:32 +0000

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import numpy as np
+import matplotlib.pyplot as mplt
+
+root = Tk()
+root.title("My Code")
+root.iconbitmap('c:/Users/corru/OneDrive/Documents/Python/GUI')
+root.geometry("400x400")
+
+def graph():
+    housesPrices = np.random.normal(20000, 25000, 5000)
+    mplt.hist(housesPrices, 50)
+    mplt.show()
+
+myButton = Button(root, text="House Graphs", command=graph)
+myButton.pack()
+
+
+# Execution
+root.mainloop()
